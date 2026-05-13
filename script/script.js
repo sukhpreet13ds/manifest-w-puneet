1eb364a14e1c8431c0b62cb13c3e676cb750fc35
sukhpreet001007 <sukhpreet001007@gmail.com>
2026-05-13 17:04:12 +0530

    Buttoons and shades and marquee track

--- script/script.js
+++ script/script.js
@@ -6,3 +6,14 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('scrolled');
     }
 });
+
+// Pause marquee on hover
+const marquee = document.querySelector('.mindset-marquee-track');
+
+marquee.addEventListener('mouseenter', () => {
+    marquee.style.animationPlayState = 'paused';
+});
+
+marquee.addEventListener('mouseleave', () => {
+    marquee.style.animationPlayState = 'running';
+});
